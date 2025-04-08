bdbbadf3ebf6d7281cd599da2186e1c5df69ad11
Oleksandr Semeniuk <ovsmnk@gmail.com>
2025-04-08 15:38:32 +0300

    added dashboard neovim plugin

---
+++ nvim/.config/nvim/lua/plugins/dashboard.lua
@@ -0,0 +1,41 @@
+return {
+	"nvimdev/dashboard-nvim",
+	event = "VimEnter",
+	config = function()
+		require("dashboard").setup({
+			theme = "hyper",
+			config = {
+				week_header = {
+					enable = true,
+				},
+				shortcut = {
+					{ desc = "󰚰 update", group = "@property", action = "Lazy update", key = "u" },
+					{
+						icon = " ",
+						icon_hl = "@variable",
+						desc = "files",
+						group = "Label",
+						action = "Telescope find_files",
+						key = "f",
+					},
+					-- {
+					-- 	desc = " vault",
+					-- 	group = "DiagnosticHint",
+					-- 	action = "Telescope app",
+					-- 	key = "v",
+					-- },
+					{
+						desc = " dotfiles",
+						group = "Number",
+						action = function()
+							vim.cmd("cd $HOME/dotfiles")
+							vim.cmd("Neotree")
+						end,
+						key = "d",
+					},
+				},
+			},
+		})
+	end,
+	dependencies = { { "nvim-tree/nvim-web-devicons" } },
+}
